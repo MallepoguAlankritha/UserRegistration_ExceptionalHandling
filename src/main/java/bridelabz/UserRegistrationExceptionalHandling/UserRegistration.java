9c48bc2585732fa43f15352e0ae6e80a5b284859
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-26 11:36:04 +0530

    Added:User need to enter a valid first name

--- src/main/java/bridelabz/UserRegistrationExceptionalHandling/UserRegistration.java
+++ src/main/java/bridelabz/UserRegistrationExceptionalHandling/UserRegistration.java
@@ -1,9 +1,16 @@
 package bridelabz.UserRegistrationExceptionalHandling;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+/**
+ * Purpose  - As a User need to enter a valid First Name
+ */
 
 public class UserRegistration {
 
-	public static void main(String[] args) {
-		System.out.println("welcome");
-	}
-
+    public boolean firstName(String firstName) {
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
 }
--- src/test/java/bridelabz/UserRegistrationExceptionalHandling/UserRegistrationTest.java
+++ src/test/java/bridelabz/UserRegistrationExceptionalHandling/UserRegistrationTest.java
@@ -1,19 +1,27 @@
 package bridelabz.UserRegistrationExceptionalHandling;
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for validating first name
  */
-public class UserRegistrationTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class UserRegistrationTest {
+    UserRegistration userRegistration;
+    @Before
+    public void before() {
+        userRegistration = new UserRegistration();
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        boolean result = userRegistration.firstName("Mallepogu");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenFirstName_WhenNotProper_ShouldReturnFlase() {
+        boolean result = userRegistration.firstName("mallepogu");
+        Assert.assertEquals(false, result);
     }
 }
